2d3ead108f63e1d044115c33d9bacc00c26b3d2c
Brian Scherner <brianscherner@gmail.com>
2023-05-09 14:47:15 -0700

    Add eventListener to load page and define div classes. Add eventListener for submit function declaration. Define each triangle side. Set attribute for each div class upon form submission. Write 'if' statement defining parameters for what will determine each kind of triangle.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,31 @@
-function triangleType(event) {
-	event.preventDefault();
-	const sideA = document.getElementById("A").value;
-	const sideB = document.getElementById("B").value;
-	const sideC = document.getElementById("C").value;
-}
+window.addEventListener("load", function() {
+	let form = document.querySelector("form");
+	let equilateral = document.getElementById("equilateral");
+	let isosceles = document.getElementById("isosceles");
+	let scalene = document.getElementById("scalene");
+	let none = document.getElementById("none");
 
-	if (sideA && sideB && sideC) {
-		if (sideA === sideB || number2 === number1) {}
-	}
+	form.addEventListener("submit", function(event){
+		const A = document.getElementById("A").value;
+		const B = document.getElementById("B").value;
+		const C = document.getElementById("C").value;
+
+		equilateral.setAttribute("class", "hidden");
+		isosceles.setAttribute("class", "hidden");
+		scalene.setAttribute("class", "hidden");
+		none.setAttribute("class", "hidden");
+		event.preventDefault();
+
+		if (A && B && C) {
+			if (A === B && B === C) {
+				document.getElementById("equilateral").removeAttribute("class");
+			} else if (A === B && C !== B) {
+					document.getElementById("isosceles").removeAttribute("class");
+			} else if (A !== B || C !== B) {
+					document.getElementById("scalene").removeAttribute("class");
+			} else {
+					document.getElementById("none").removeAttribute("class");
+			}
+		}
+	});
+});
