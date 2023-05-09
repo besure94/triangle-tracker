bdd3fc57de3b60d00313daac9f9227e2c9ba14dd
Brian Scherner <brianscherner@gmail.com>
2023-05-09 15:43:42 -0700

    Define variable for resetBtn. Add eventListener for resetBtn removing attribute when button is submitted. Add eventListener for resetBtn disappearing div classes and nullifying form values when clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", function() {
 	let form = document.querySelector("form");
+	let resetBtn = document.getElementById("reset");
 	let equilateral = document.getElementById("equilateral");
 	let isosceles = document.getElementById("isosceles");
 	let scalene = document.getElementById("scalene");
@@ -24,8 +25,22 @@ window.addEventListener("load", function() {
 			} else if (A !== B || C !== B) {
 					document.getElementById("scalene").removeAttribute("class");
 			} else {
-					document.getElementById("none").removeAttribute("class");
+
 			}
 		}
+	});
+
+	form.addEventListener("submit", function() {
+		resetBtn.removeAttribute("class");
+	});
+
+	resetBtn.addEventListener("click", function(){
+		equilateral.setAttribute("class", "hidden");
+		isosceles.setAttribute("class", "hidden");
+		scalene.setAttribute("class", "hidden");
+		none.setAttribute("class", "hidden");
+		document.getElementById("A").value = null;
+		document.getElementById("B").value = null;
+		document.getElementById("C").value = null;
 	});
 });
