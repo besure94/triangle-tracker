d35f6752ae2749cd1025186190a0cca7f94b4b79
Brian Scherner <brianscherner@gmail.com>
2023-05-09 13:05:13 -0700

    Change function to triangleType. Change variables to match input id's from index.html.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function triangleType(event) {
+	event.preventDefault();
+	const sideA = document.getElementById("A").value;
+	const sideB = document.getElementById("B").value;
+	const sideC = document.getElementById("C").value;
+}
+
+	if (sideA && sideB && sideC) {
+		if (sideA === sideB || number2 === number1) {}
+	}
